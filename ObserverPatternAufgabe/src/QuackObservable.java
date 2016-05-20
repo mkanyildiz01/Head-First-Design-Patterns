52508f71f0cb61d2ea642183b6b98d097b38aa52
mkanyildiz01-TGM <mkanyildiz01@student.tgm.ac.at>
2016-05-20 14:32:44 +0200

    QuackObservable JavaDoc

--- ObserverPatternAufgabe/src/QuackObservable.java
+++ ObserverPatternAufgabe/src/QuackObservable.java
@@ -1,6 +1,18 @@
+/**
+*	Class-Name: QuackObservable.java
+*	Name: Kanyildiz Muhammedhizir
+*	Klasse: 4AHITM
+*	Datum: 15.05.2016
+**/
 
 package headfirst.designpatterns.combining.observer;
 
+/**
+*
+*	Diese Klasse ist ein Interface.
+*	Es beeinhaltet 2 Methoden.
+*
+**/
 public interface QuackObservable {
 	public void registerObserver(Observer observer);
 	public void notifyObservers();
