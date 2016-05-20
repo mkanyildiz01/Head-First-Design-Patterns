c2c9fd924779158e5c9cbaf4e6d557b7ab6379c3
mkanyildiz01-TGM <mkanyildiz01@student.tgm.ac.at>
2016-05-20 14:27:34 +0200

    Observer JavaDoc Fertig

--- ObserverPatternAufgabe/src/Observer.java
+++ ObserverPatternAufgabe/src/Observer.java
@@ -1,5 +1,17 @@
+/**
+*	Class-Name: Observer.java
+*	Name: Kanyildiz Muhammedhizir
+*	Klasse: 4AHITM
+*	Datum: 15.05.2016
+**/
+
 package headfirst.designpatterns.combining.observer;
 
+/**
+*	Observer ist ein Interface.
+*	Dieses Interface deffiniert eine Methode update mit dem Parameter duck dessen Typ QuackObservable ist.
+*	Diese Methode wird in Quackologist verwendet.
+**/
 public interface Observer {
 	public void update(QuackObservable duck);
 }
