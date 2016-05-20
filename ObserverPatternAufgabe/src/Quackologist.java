a9aadda47872123bc25b6aece9b4da68e8d60b42
mkanyildiz01-TGM <mkanyildiz01@student.tgm.ac.at>
2016-05-20 14:15:40 +0200

    Quackologist JavaDoc fertig

--- ObserverPatternAufgabe/src/Quackologist.java
+++ ObserverPatternAufgabe/src/Quackologist.java
@@ -1,11 +1,38 @@
+/**
+*	Class-Name: Quackologist.java
+*	Name: Kanyildiz Muhammedhizir
+*	Klasse: 4AHITM
+*	Datum: 15.05.2016
+**/
+
 package headfirst.designpatterns.combining.observer;
 
+/**
+*	Pattern: Factory/Decorator Method
+*	Beschreibung: 	Die Klasse Quackologist ist die einzige Observer (Beobachter)-Klasse. Alle Enten werden zur 
+*					Observable-Klasse referenziert, welche die Quackologist-Klasse mittels der Methode update() 
+*					benachrichtigt.
+*					
+**/
+
 public class Quackologist implements Observer {
- 
+	
+	/**
+	*	Methoden Name: update
+	*
+	*	Diese Methode Gibt eine Nachricht in die Konsole aus. Diese Nachricht bekommt es als Parameter geliefert.
+	**/
+	
 	public void update(QuackObservable duck) {
 		System.out.println("Quackologist: " + duck + " just quacked.");
 	}
- 
+	
+ 	/**
+	*	Methoden Name: toString
+	*
+	*	Rückgabe: String: 'Quackologiest'
+	**/
+	
 	public String toString() {
 		return "Quackologist";
 	}
