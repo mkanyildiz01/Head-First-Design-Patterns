b48f49af8339c481425b8e7e1a9ca95ff1358c72
mkanyildiz01-TGM <mkanyildiz01@student.tgm.ac.at>
2016-05-20 13:31:10 +0200

    Quackable JavaDoc Fertig

--- ObserverPatternAufgabe/src/Quackable.java
+++ ObserverPatternAufgabe/src/Quackable.java
@@ -1,4 +1,19 @@
+/**
+*	Class-Name: Quackable.java
+*	Name: Kanyildiz Muhammedhizir
+*	Klasse: 4AHITM
+*	Datum: 15.05.2016
+**/
+
 package headfirst.designpatterns.combining.observer;
+
+/**
+*	Pattern: Factory/Decorator Method
+*	Beschreibung: 	Diese Klasse erbt von der Klasse QuackObservable
+*					Somit Überschreibt es die Methoden der Klasse QuackObservable.
+*					Diese Klasse ruft die Methode quack auf.
+*					
+**/
 
 public interface Quackable extends QuackObservable {
 	public void quack();
