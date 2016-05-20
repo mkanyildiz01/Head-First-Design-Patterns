3b47a0c71a4a30877c78d191a8130fa3f9271d7c
mkanyildiz01-TGM <mkanyildiz01@student.tgm.ac.at>
2016-05-20 13:16:08 +0200

    Method-Factory Klassen Fertig

--- ObserverPatternAufgabe/src/RedheadDuck.java
+++ ObserverPatternAufgabe/src/RedheadDuck.java
@@ -1,24 +1,72 @@
-package headfirst.designpatterns.combining.observer;
+/**
+*	Class-Name: RedheadDuck.java
+*	Name: Kanyildiz Muhammedhizir
+*	Klasse: 4AHITM
+*	Datum: 15.05.2016
+**/
 
+package headfirst.designpatterns.combining.observer;
+/**
+*	Pattern: Factory Method
+*	Beschreibung: 	Diese Klasse implementiert vom Interface Quackable.
+*					Somit enthlt es alle Attribute und Methoden(1 Methode in unserem Fall) von Quackable.
+*					
+**/
 public class RedheadDuck implements Quackable {
+	
+	// Globale Variable: observable von der Klasse Observable
 	Observable observable;
 
+	/**
+	*	Methoden Name: RedheadDuck
+	*
+	*	Es erstellt eine neue Instanz von Observable mit dem Parameter observable
+	**/
 	public RedheadDuck() {
 		observable = new Observable(this);
 	}
-
+ 
+	/**
+	*	Methoden Name: quack
+	*
+	*	Es gib ein String 'Quack' in die Konsole aus.
+	*	Dannach ruft es die Methode notifyObservers auf.
+	*	
+	**/
+	
 	public void quack() {
 		System.out.println("Quack");
 		notifyObservers();
 	}
-
+	
+	/**
+	*	Methoden Name: registerObserver
+	*
+	*	Es hat als Parameter einen observer
+	*	In dieser Methode wird die Methode registerObserver mit dem Parameter observer aufgerufen. 
+	*	Die Methode registerOberserver befindet sich in observable
+	**/
+	
 	public void registerObserver(Observer observer) {
 		observable.registerObserver(observer);
 	}
 
+	/**
+	*	Methoden Name: notifyObservers
+	*
+	*	Diese Methode ruft die Methode notifyObservers von der Klasse Observable auf.
+	**/
+	
 	public void notifyObservers() {
 		observable.notifyObservers();
 	}
+	
+	/**
+	*	Methoden Name: toString
+	*
+	*	Diese Methode ist ein String und hat den Rückgabetypen: String
+	*	Es gibt den Namen der Ente als String aus: 'Redhead Duck'
+	**/
 
 	public String toString() {
 		return "Redhead Duck";
--- ObserverPatternAufgabe/src/RubberDuck.java
+++ ObserverPatternAufgabe/src/RubberDuck.java
@@ -1,25 +1,73 @@
+/**
+*	Class-Name: RubberDuck.java
+*	Name: Kanyildiz Muhammedhizir
+*	Klasse: 4AHITM
+*	Datum: 15.05.2016
+**/
 package headfirst.designpatterns.combining.observer;
 
+/**
+*	Pattern: Factory Method
+*	Beschreibung: 	Diese Klasse implementiert vom Interface Quackable.
+*					Somit enthlt es alle Attribute und Methoden(1 Methode in unserem Fall) von Quackable.
+*					
+**/
+
 public class RubberDuck implements Quackable {
+	
+	// Globale Variable: observable von der Klasse Observable
 	Observable observable;
 
+	/**
+	*	Methoden Name: RubberDuck
+	*
+	*	Es erstellt eine neue Instanz von Observable mit dem Parameter observable
+	**/
 	public RubberDuck() {
 		observable = new Observable(this);
 	}
  
+	/**
+	*	Methoden Name: quack
+	*
+	*	Es gib ein String 'Squeak' in die Konsole aus.
+	*	Dannach ruft es die Methode notifyObservers auf.
+	*	
+	**/
 	public void quack() {
 		System.out.println("Squeak");
 		notifyObservers();
 	}
-
+	
+	/**
+	*	Methoden Name: registerObserver
+	*
+	*	Es hat als Parameter einen observer
+	*	In dieser Methode wird die Methode registerObserver mit dem Parameter observer aufgerufen. 
+	*	Die Methode registerOberserver befindet sich in observable
+	**/
+	
 	public void registerObserver(Observer observer) {
 		observable.registerObserver(observer);
 	}
-
+	
+	/**
+	*	Methoden Name: notifyObservers
+	*
+	*	Diese Methode ruft die Methode notifyObservers von der Klasse Observable auf.
+	**/
+	
 	public void notifyObservers() {
 		observable.notifyObservers();
 	}
-  
+  	
+	/**
+	*	Methoden Name: toString
+	*
+	*	Diese Methode ist ein String und hat den Rückgabetypen: String
+	*	Es gibt den Namen der Ente als String aus: 'Rubber Duck'
+	**/
+ 
 	public String toString() {
 		return "Rubber Duck";
 	}
