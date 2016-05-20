c5398ab3206bccfd70dbc34bc51a2ea1c03b82c8
mkanyildiz01-TGM <mkanyildiz01@student.tgm.ac.at>
2016-05-20 14:43:54 +0200

    Flock javaDoc Fertig

--- ObserverPatternAufgabe/src/Flock.java
+++ ObserverPatternAufgabe/src/Flock.java
@@ -1,15 +1,43 @@
+/**
+*	Class-Name: Flock.java
+*	Name: Kanyildiz Muhammedhizir
+*	Klasse: 4AHITM
+*	Datum: 15.05.2016
+**/
+
 package headfirst.designpatterns.combining.observer;
+
+/**
+*
+*	Diese Klasse Implementiert das Interface Quackable
+*					
+**/
 
 import java.util.Iterator;
 import java.util.ArrayList;
 
 public class Flock implements Quackable {
+	// Klassen ArrayList ducks mit dem Typen Quackable
 	ArrayList<Quackable> ducks = new ArrayList<Quackable>();
   
+	/**
+	* Methoden Name: add
+	*
+	* Diese Methode hat einen Parameter mit dem Tyen Quackable mit dem namen duck
+	* Dieser duck wird in die Arraylist hinzugefügt.
+	**/
+	
 	public void add(Quackable duck) {
 		ducks.add(duck);
 	}
-  
+  	
+	/**
+	*	Methoden Name: quack
+	*
+	*	Hier befindet sich ein Iterator mit dem man alle elemente vom Arraylist durchgehen kann und dies auch gemacht wird.
+	*	Es wird noch dazu überprüft.
+	*	Es hat außerdem noch einen Parameter verlangt, dammit es etwas adden kann.
+	**/
 	public void quack() {
 		Iterator<Quackable> iterator = ducks.iterator();
 		while (iterator.hasNext()) {
@@ -17,7 +45,13 @@ public class Flock implements Quackable {
 			duck.quack();
 		}
 	}
-   
+   	/**
+	*	Methoden Name: registerObserver
+	*
+	*	Hier befindet sich ein Iterator mit dem man alle elemente vom Arraylist durchgehen kann und dies auch gemacht wird.
+	*	Es wird noch dazu überprüft.
+	*	Es hat außerdem noch einen Parameter verlangt, dammit es etwas adden kann.
+	**/
 	public void registerObserver(Observer observer) {
 		Iterator<Quackable> iterator = ducks.iterator();
 		while (iterator.hasNext()) {
@@ -27,7 +61,12 @@ public class Flock implements Quackable {
 	}
   
 	public void notifyObservers() { }
-  
+	
+	/**
+	*	Methoden Name: toString
+	*
+	*	Rückgabe: String: 'Flock of Ducks'
+	**/
 	public String toString() {
 		return "Flock of Ducks";
 	}
